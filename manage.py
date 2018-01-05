2e55d4bbb7048804e8f5ba9ac5982a8efe65169e
alee policarpio <aleeciaralpolicarpio@gmail.com>
2018-01-05 12:13:18 +0800

    Add production config

--- manage.py
+++ manage.py
@@ -9,12 +9,10 @@ def __runserver():
 
 parser = argparse.ArgumentParser(description='Application commands.')
 parser.add_argument('command', default=None)
-parser.add_argument('--env', default='dev')
 
 if __name__ == '__main__':
     args = parser.parse_args()
     command = args.command
-    analytics.app.env = args.env
 
     if command == 'runserver':
         __runserver()
