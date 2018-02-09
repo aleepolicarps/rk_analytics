7903605cfcbc0f808cb0b5a9ffdd6e96c4219666
alee policarpio <aleeciaralpolicarpio@gmail.com>
2018-02-09 17:16:27 +0800

    Deprecate table replication for FB

--- analytics/scheduled_tasks/__init__.py
+++ analytics/scheduled_tasks/__init__.py
@@ -14,15 +14,21 @@ scheduler.start()
 transactions_replicator = TransactionsReplicator()
 chargebacks_replicator = ChargebacksReplicator()
 
-scheduler.add_job(chargebacks_replicator.replicate_sd_chargebacks, 'interval', minutes=5)
-scheduler.add_job(chargebacks_replicator.replicate_fb_chargebacks, 'interval', minutes=5)
-scheduler.add_job(chargebacks_replicator.replicate_bb_chargebacks, 'interval', minutes=5)
-scheduler.add_job(chargebacks_replicator.replicate_pb_chargebacks, 'interval', minutes=5)
+def __replicate_pb_tables():
+    transactions_replicator.replicate_pb_transactions()
+    chargebacks_replicator.replicate_pb_chargebacks()
 
-scheduler.add_job(transactions_replicator.replicate_fb_transactions, 'interval', minutes=5)
-scheduler.add_job(transactions_replicator.replicate_pb_transactions, 'interval', minutes=5)
-scheduler.add_job(transactions_replicator.replicate_bb_transactions, 'interval', minutes=5)
-scheduler.add_job(transactions_replicator.replicate_sd_transactions, 'interval', minutes=5)
+def __replicate_bb_tables():
+    transactions_replicator.replicate_bb_transactions()
+    chargebacks_replicator.replicate_bb_chargebacks()
+
+def __replicate_sd_tables():
+    transactions_replicator.replicate_sd_transactions()
+    chargebacks_replicator.replicate_sd_chargebacks()
+
+scheduler.add_job(__replicate_pb_tables, 'interval', minutes=10)
+scheduler.add_job(__replicate_bb_tables, 'interval', minutes=10)
+scheduler.add_job(__replicate_sd_tables, 'interval', minutes=10)
 
 scheduler.print_jobs()
 atexit.register(lambda: scheduler.shutdown())
