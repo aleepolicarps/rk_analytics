d06a171c0de9cdc09f57c87abd6b14cb4bad3d42
alee policarpio <aleeciaralpolicarpio@gmail.com>
2018-02-12 16:55:41 +0800

    Add migrations for local env

--- migrations/20170129-create_transactions_table.sql
+++ migrations/20170129-create_transactions_table.sql
@@ -1,3 +1,41 @@
+-- dev --
+CREATE TABLE transactions(
+    id SERIAL PRIMARY KEY NOT NULL,
+    account VARCHAR(255),
+    merchant_user_id VARCHAR(255),
+    transaction_type VARCHAR(120),
+    mode VARCHAR(15),
+    code INT,
+    amount REAL,
+    currency VARCHAR(15),
+    card_holder VARCHAR(255),
+    brand VARCHAR(255),
+    bank VARCHAR(255),
+    level VARCHAR(255),
+    type VARCHAR(255),
+    bin VARCHAR(255),
+    last VARCHAR(255),
+    exp_month VARCHAR(255),
+    exp_year VARCHAR(255),
+    bank_id INT,
+    bank_authcode VARCHAR(255),
+    bank_time VARCHAR(255),
+    charge_time VARCHAR(255),
+    token VARCHAR(255),
+    reference VARCHAR(255),
+    base_reference VARCHAR(255),
+    transaction_unique_id VARCHAR(255),
+    fraudulent BOOLEAN default false,
+    created_at TIMESTAMPTZ,
+    response TEXT,
+    webid VARCHAR(15),
+    country VARCHAR (255),
+    original_id INT,
+    status VARCHAR(120)
+);
+-- end dev --
+
+-- production --
 CREATE TABLE transactions(
     id INT IDENTITY(1, 1) NOT NULL,
     account VARCHAR(255),
@@ -32,3 +70,4 @@ CREATE TABLE transactions(
     original_id INT,
     status VARCHAR(120)
 );
+-- end production --
--- migrations/20180209-create_chargebacks_table.sql
+++ migrations/20180209-create_chargebacks_table.sql
@@ -1,3 +1,31 @@
+-- dev --
+CREATE TABLE chargebacks(
+    id SERIAL PRIMARY KEY NOT NULL,
+    account VARCHAR(255),
+    merchant_user_id VARCHAR(255),
+    webid VARCHAR(15),
+    country VARCHAR (255),
+    original_id INT,
+    status VARCHAR(120),
+    type VARCHAR(120),
+    mode VARCHAR(15),
+    amount REAL,
+    currency VARCHAR(15),
+    bank_id INT,
+    bank_authcode VARCHAR(255),
+    bank_time VARCHAR(255),
+    bank_update_time VARCHAR(255),
+    reference VARCHAR(255),
+    base_reference VARCHAR(255),
+    transaction_unique_id VARCHAR(255),
+    created_at TIMESTAMPTZ,
+    custom_mid_name VARCHAR(255),
+    response TEXT,
+    time VARCHAR(255)
+);
+-- end dev --
+
+-- production --
 CREATE TABLE chargebacks(
     id INT IDENTITY(1, 1) NOT NULL,
     account VARCHAR(255),
@@ -22,3 +50,4 @@ CREATE TABLE chargebacks(
     response varchar(MAX),
     time VARCHAR(255)
 );
+-- end production --
