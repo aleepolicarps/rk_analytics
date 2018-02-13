80396cf64c9f61a35dc0dca1f029261ef325d4c3
alee policarpio <aleeciaralpolicarpio@gmail.com>
2018-02-13 14:25:50 +0800

    Update time for forexrate getter

--- analytics/scheduled_tasks/forex_rate_getter.py
+++ analytics/scheduled_tasks/forex_rate_getter.py
@@ -3,6 +3,8 @@ from analytics.models import ForExRates
 from sqlalchemy import update
 import json
 import requests
+from datetime import datetime
+import pytz
 
 
 class ForExRateGetter:
@@ -12,10 +14,11 @@ class ForExRateGetter:
         response = requests.get('https://api.fixer.io/latest?base=EUR')
         data = response.json()
         rates = data['rates']
+        now = datetime.now(pytz.timezone('Europe/Copenhagen'))
 
         for currency, rate in rates.iteritems():
             forex_rate = ForExRates.select(ForExRates.c.currency == currency).execute().first()
             if forex_rate:
-                ForExRates.update().where(ForExRates.c.currency == currency).values(rate=rate).execute()
+                ForExRates.update().where(ForExRates.c.currency == currency).values(rate=rate, updated_at=now).execute()
             else:
                 db_conn.execute(ForExRates.insert(), currency=currency, rate=rate)
